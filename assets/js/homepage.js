97ead00041f42ff8770efbd3c17e189669f98544
Sadrac Tijerina <reavity3@gmail.com>
2021-05-02 23:58:21 -0500

    retructured the index page. Also am able to navigate to the team page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,2 +1,10 @@
 //ADD NBA TEAM ID AT THE END
 var NBAPI = "https://www.balldontlie.io/api/v1/teams/";
+
+var TEAMKEY;
+
+$(".container").on("click", "img", function () {
+  TEAMKEY = this.id;
+
+  document.location.replace("./teampage.html");
+});
--- assets/js/teampage.js
+++ assets/js/teampage.js
@@ -1,2 +1,5 @@
-var wikiAPI = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+var wikiAPI =
+  "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=";
 // %20 indicates a space in the URL
+
+console.log(TEAMKEY);
