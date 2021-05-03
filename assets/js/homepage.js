9f07533e180bd4f78fb030aaa341012839ed444c
Sadrac Tijerina <reavity3@gmail.com>
2021-05-03 01:22:30 -0500

    minor changes to team and homepage js files

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,8 @@
-//ADD NBA TEAM ID AT THE END
-var NBAPI = "https://www.balldontlie.io/api/v1/teams/";
-
 var TEAMKEY;
 
 $(".container").on("click", "img", function () {
   TEAMKEY = this.id;
 
+  //document.location.replace("./teampage.html&id=" + TEAMKEY); RESPONSIVE
   document.location.replace("./teampage.html");
 });
